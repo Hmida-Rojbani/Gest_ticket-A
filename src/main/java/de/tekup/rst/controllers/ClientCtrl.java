e295eaab7d55a8b1846ce4dda1dded51cd7cff69
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-11-09 14:49:53 +0100

    update DTO in

--- src/main/java/de/tekup/rst/controllers/ClientCtrl.java
+++ src/main/java/de/tekup/rst/controllers/ClientCtrl.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import de.tekup.rst.dto.models.ClientReqDTO;
 import de.tekup.rst.entities.Client;
 import de.tekup.rst.services.ClientService;
 import lombok.AllArgsConstructor;
@@ -16,7 +17,7 @@ public class ClientCtrl {
 	
 	private ClientService clientService;
 	@PostMapping("/api/clients")
-	public Client addClient(@RequestBody Client client) {
+	public Client addClient(@RequestBody ClientReqDTO client) {
 		return clientService.saveToDB(client);
 	}
 
--- src/main/java/de/tekup/rst/services/ClientService.java
+++ src/main/java/de/tekup/rst/services/ClientService.java
@@ -2,6 +2,7 @@ package de.tekup.rst.services;
 
 import org.springframework.stereotype.Service;
 
+import de.tekup.rst.dto.models.ClientReqDTO;
 import de.tekup.rst.entities.Client;
 import de.tekup.rst.repositories.ClientRepository;
 import lombok.AllArgsConstructor;
@@ -12,7 +13,13 @@ public class ClientService {
 	
 	private ClientRepository clientRepository;
 	
-	public Client saveToDB(Client client) {
+	public Client saveToDB(ClientReqDTO clientDto) {
+		Client client = new Client();
+		client.setNom(clientDto.getNom());
+		client.setPrenom(clientDto.getPrenom());
+		client.setDateDeNaissance(clientDto.getDateDeNaissance());
+		client.setCourriel(clientDto.getCourriel());
+		client.setTelephone(clientDto.getTelephone());
 		return clientRepository.save(client);
 	}
 
