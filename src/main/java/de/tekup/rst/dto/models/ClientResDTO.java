748d0a6fb8934eb49b3fdf7def9d23dafafe0bdc
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-11-09 15:34:57 +0100

    upate Res

---
+++ src/main/java/de/tekup/rst/dto/models/ClientResDTO.java
@@ -0,0 +1,12 @@
+package de.tekup.rst.dto.models;
+
+import lombok.Data;
+
+@Data
+public class ClientResDTO {
+	
+	private Integer id;
+	private String nomComplet;
+	private int age;
+
+}
--- src/main/java/de/tekup/rst/services/ClientService.java
+++ src/main/java/de/tekup/rst/services/ClientService.java
@@ -1,8 +1,12 @@
 package de.tekup.rst.services;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 import org.springframework.stereotype.Service;
 
 import de.tekup.rst.dto.models.ClientReqDTO;
+import de.tekup.rst.dto.models.ClientResDTO;
 import de.tekup.rst.entities.Client;
 import de.tekup.rst.repositories.ClientRepository;
 import lombok.AllArgsConstructor;
@@ -13,14 +17,20 @@ public class ClientService {
 	
 	private ClientRepository clientRepository;
 	
-	public Client saveToDB(ClientReqDTO clientDto) {
+	public ClientResDTO saveToDB(ClientReqDTO clientDto) {
 		Client client = new Client();
 		client.setNom(clientDto.getNom());
 		client.setPrenom(clientDto.getPrenom());
 		client.setDateDeNaissance(clientDto.getDateDeNaissance());
 		client.setCourriel(clientDto.getCourriel());
 		client.setTelephone(clientDto.getTelephone());
-		return clientRepository.save(client);
+		clientRepository.save(client);
+		ClientResDTO clientResDTO = new ClientResDTO();
+		clientResDTO.setId(client.getId());
+		clientResDTO.setNomComplet(client.getPrenom()+" "+client.getNom());
+		clientResDTO.setAge((int)ChronoUnit.YEARS.between(client.getDateDeNaissance(),
+				LocalDate.now()));
+		return clientResDTO;
 	}
 
 }
