b7721b812f7c5099d69c7c2411a15f5439d024f6
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-11-09 13:52:34 +0100

    Update ClientCtrl.java

--- src/main/java/de/tekup/rst/controllers/ClientCtrl.java
+++ src/main/java/de/tekup/rst/controllers/ClientCtrl.java
@@ -2,6 +2,7 @@ package de.tekup.rst.controllers;
 
 
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,7 @@ import lombok.AllArgsConstructor;
 public class ClientCtrl {
 	
 	private ClientService clientService;
-	
+	@PostMapping("/api/clients")
 	public Client addClient(@RequestBody Client client) {
 		return clientService.saveToDB(client);
 	}
