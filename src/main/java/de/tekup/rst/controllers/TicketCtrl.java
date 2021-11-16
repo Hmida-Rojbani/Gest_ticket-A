20d7778fb7b1d78f7554f2f2a9af1263948329d5
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-11-16 16:00:52 +0100

    Update TicketCtrl.java

--- src/main/java/de/tekup/rst/controllers/TicketCtrl.java
+++ src/main/java/de/tekup/rst/controllers/TicketCtrl.java
@@ -1,5 +1,24 @@
 package de.tekup.rst.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import de.tekup.rst.dto.models.TicketDTO;
+import de.tekup.rst.services.TicketService;
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/tickets")
 public class TicketCtrl {
+	
+	private TicketService ticketService;
+	
+	@PostMapping
+	public double createTicket(@RequestBody TicketDTO ticketDTO) {
+		return ticketService.create(ticketDTO);
+	}
 
 }
